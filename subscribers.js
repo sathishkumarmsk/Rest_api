31697bb19c1ef214f4ed79954b3d56548e692358
sathishkumarmsk <sathishkumarmsk5699@gmail.com>
2022-01-12 12:00:17 +0530

    get data

--- subscribers.js
+++ subscribers.js
@@ -1,12 +1,32 @@
 const express = require('express')
 const router = express.Router()
+const Subscriber = require('../models/subscriber.js')
 
-router.get('/', (req, res) => {
-    res.send('Hello World')
+router.get('/', async (req, res) => {
+    try {
+        const subscribers = await Subscriber.find()
+        res.json(subscribers)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+
+    }
+    
+    
 
 })
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+    const subscriber = new Subscriber({
+        name: req.body.name,
+        subscribedChannel: req.body.subscribedChannel
+    }) 
+
+    try {
+        const newSubscriber = await subscriber.save()
+        res.status(201).json(newSubscriber)
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }
 
 })
 
